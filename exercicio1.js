2722ffd3a752d242bce2068c9fdde8ebbed9142b
MarceloFCandido <fcandido.marcelo@gmail.com>
2021-11-22 14:57:37 -0300

    doing first exercise

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,12 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+document.getElementById('calcular').addEventListener('click', function (e) {
+    let G = Number(document.getElementById('constante').value)
+    let M1 = Number(document.getElementById('massa1').value)
+    let M2 = Number(document.getElementById('massa2').value)
+    let d = Number(document.getElementById('distancia').value)
+
+    let Fgrav = (G * M1 * M2) / (d * d)
+    document.getElementById('resultado').value = Fgrav
+})
+
+
